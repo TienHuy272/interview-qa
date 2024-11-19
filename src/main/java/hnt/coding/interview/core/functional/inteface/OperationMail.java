db5b83ea6e29b3e7b1a7bd8635b6768ff63671bf
HuyNT <tienhuylionel@gmail.com>
2024-11-19 14:39:33 +0700

    add more practice

--- src/main/java/hnt/coding/interview/core/functional/inteface/OperationMail.java
+++ src/main/java/hnt/coding/interview/core/functional/inteface/OperationMail.java
@@ -1,5 +1,10 @@
 package hnt.coding.interview.core.functional.inteface;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class OperationMail {
     public static void main(String[] args) {
         int a = 5;
@@ -12,5 +17,18 @@ public class OperationMail {
         String x2 = "huy";
         Operation<String> operation2 = "%s - %s"::formatted;
         System.out.println(operation2.calculate(x1, x2));
+
+        Predicate<Integer> compareInt = x -> x > 10;
+        System.out.println(compareInt.test(100));
+
+        Consumer<String> consumer = s -> System.out.println(s);
+        consumer.accept("show me");
+
+        Supplier<String> supplier = () ->  "001";
+        System.out.println(supplier.get());
+
+        Function<String, Integer> function = (y) -> y.length();
+        System.out.println(function.apply("123"));
+
     }
 }
